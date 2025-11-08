9e40d92956844b5493775f64d84dd1ea6b38c325
Joao fini <joaovoto86@gmail.com>
2025-11-08 11:41:48 -0300

    começando a desenvolver o chat geral e o menu de opçoes

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 import sqlite3
 import questionary
+
 user = sqlite3.connect("usuarios.db")
+
+os.makedirs("chat", exist_ok=True)   
+
 
 cursor = user.cursor()
 cursor.execute("""
@@ -13,6 +17,13 @@ CREATE TABLE IF NOT EXISTS usuarios(
 
 cursor.execute("""CREATE UNIQUE INDEX IF NOT EXISTS id_user ON usuarios(nome)""")
 user.commit()
+
+cursor.execute("SELECT id FROM usuarios")
+lista = cursor.fetchall()
+for i in range(1,(len(lista)+1)):
+    e = str(i)
+    os.makedirs("chat/"+e,exist_ok=True)
+
 def login():
     nome = input("Digite seu nome de usuario: ")
     senha = input("Digite sua senha: ")
@@ -21,7 +32,9 @@ def login():
     result = logon.fetchone()
     if result:
         print(f"Bem vindo de volta {nome}")
-        return True
+        cursor.execute("SELECT id FROM usuarios WHERE nome = ?",(nome,))
+        id_u = cursor.fetchone()
+        return id_u
     else:
         print("Usuario não encontrado")
     # verificar se senha=senha
@@ -47,6 +60,29 @@ def cadastro():
                 return login()
             else:
                 continue
-cadastro()
-os.system("cls")
-login()
+
+def menu(id_user):
+    escolha = questionary.select("",
+            choices=[
+                "Ver conversas",
+                "Adicionar conversas",
+                "Sair"
+            ]).ask()
+    if escolha.startswith("V"):
+        print("legal")
+    elif escolha.startswith("A"):
+        add_user = input("qual o ID ou nome do usuario que quer se conectar ")
+        cursor.execute("SELECT id OR nome FROM usuarios WHERE nome = ? OR id = ?",(add_user,add_user))
+        achar = cursor.fetchone()
+        if achar:
+            if not os.path.exists(id_user + "/" + add_user + ".txt"):
+                open("chat/"+id_user + "/" + add_user + ".txt", "a").close()
+        else:
+            print("usuario não existe")
+        print(achar)
+    else:
+        return False
+id_user = login()
+id_user = str(id_user[0])
+if id_user:
+    menu(id_user)
